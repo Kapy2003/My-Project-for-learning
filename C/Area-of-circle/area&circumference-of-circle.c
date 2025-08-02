8c02d44c69689449887d0c29b5552fe4eb232ffd
Kapy <93550898+Kapy2003@users.noreply.github.com>
2025-08-02 14:08:26 +0530

    No.3

--- C/Area-of-circle/area&circumference-of-circle.c
+++ C/Area-of-circle/area&circumference-of-circle.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
-#include<stdlib.h>      // this library is needed for system("pause")
+//#include<stdlib.h>      // this library is needed for system("pause")
+
 const double pi = 3.14159265359;
-int main(){
+
+int main ( void )
+{
     double r;
     printf("Enter the radius(in cms): ");
     scanf("%lf", &r);
 // area of circle
-    float area = pi*r*r;
+    double area = pi*r*r;
     printf("Area of Circle is : %f cm.sq\n", area);
 
 // circumference of circle
     double cir = pi*r*2;
     printf("Circumference of the circle is : %lf cm\n", cir);
-    system("pause");    // this is only for windows as this makes the application not quit after executing successfully
+//    system("pause");    // this is only for windows as this makes the application not quit after executing successfully
     return 0;
 }
